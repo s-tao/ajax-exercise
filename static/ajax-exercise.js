454929e87ed5db84785a3a38a1c1ebcd32cbec90
Your Name <you@example.com>
2020-02-07 13:02:20 -0800

    AJAX Javascript form in progress

---
+++ static/ajax-exercise.js
@@ -0,0 +1,54 @@
+"use strict";
+
+
+// PART 1: SHOW A FORTUNE
+
+function showFortune(evt) {
+
+    // TODO: get the fortune and show it in the #fortune-text div
+}
+
+$('#get-fortune-button').on('click', showFortune => {
+  $.get('/fortune', (res) => {
+    $('#fortune-text').html(res);
+  });
+});
+
+
+
+
+
+// PART 2: SHOW WEATHER
+
+function showWeather(evt) {
+    evt.preventDefault();
+
+    let url = "/weather.json";
+    let formData = {"zipcode": $("#zipcode-field").val()};
+
+// $('#zipcode-field').on('submit',(evt) => {
+  $.get(url, {formData}, (res) => {
+    $('#weather-form').html(res);
+  });
+// });
+
+    // TODO: request weather with that URL and show the forecast in #weather-info
+}
+
+$("#weather-form").on('submit', showWeather);
+
+
+
+
+// PART 3: ORDER MELONS
+
+function orderMelons(evt) {
+    evt.preventDefault();
+
+    // TODO: show the result message after your form
+    // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+}
+
+$("#order-form").on('submit', orderMelons);
+
+
